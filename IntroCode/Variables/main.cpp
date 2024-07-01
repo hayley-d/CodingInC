2530d9cc3a23f0eb41470750b8691a2cd0a2c872
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-07-01 08:54:39 +0200

    Initial Commit
    Revising pass by reference, value and address

---
+++ IntroCode/Variables/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int passByValue(int y) {
+    std::cout << "This is the address of y: " << &y << std::endl;
+    return y; //creates another copy of y
+}
+
+int passByAddress(int* ptr) {
+    *ptr = 8;
+    return *ptr;
+}
+
+void passByReference(int& y) {
+    y = 10;
+}
+
+int main() {
+    int x = 7; //create a local variable
+    int z = passByValue(x); // creates another copy
+
+    std::cout << "This is the address of x: " << &x << std::endl;
+    std::cout << "This is the address of z: " << &z << std::endl;
+
+    std::cout << "The value of x is " << x << std::endl;
+    z = passByAddress(&x);
+    std::cout << "The value of x is " << x << std::endl;
+    std::cout << "The address of z is " << &z << std::endl;
+
+    passByReference(x);
+    std::cout << "The value of x is " << x << std::endl;
+
+
+    return 0;
+}
