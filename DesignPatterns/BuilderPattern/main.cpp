07121994f5116ce27e9b9cf03f6c5df600fc0958
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-07-18 09:33:42 +0200

    Implement Director

--- DesignPatterns/BuilderPattern/main.cpp
+++ DesignPatterns/BuilderPattern/main.cpp
@@ -41,15 +41,39 @@ struct PizzaBuilder {
     Pizza root;
 
     //create the constructor
-    PizzaBuilder(Base b, const std::string& cheese, const std::string& sauce, Size s) {
+    /*PizzaBuilder(Base b, const std::string& cheese, const std::string& sauce, Size s) {
         root.base = b;
         root.cheese = cheese;
         root.sauce = sauce;
         root.size = s;
+    }*/
+
+    PizzaBuilder(){}
+
+    PizzaBuilder& addBase(const Base b) {
+        root.base = b;
+        return *this;
     }
 
-    void addTopping(const std::string& topping) {
+    PizzaBuilder& addCheese(const std::string& cheese) {
+        root.cheese = cheese;
+        return *this;
+    }
+
+    PizzaBuilder& addSauce(std::string sauce) {
+        root.sauce = sauce;
+        return *this;
+    }
+
+    PizzaBuilder& addSize(const Size s) {
+        root.size = s;
+        return *this;
+    }
+
+    //Fluent interface
+    PizzaBuilder& addTopping(const std::string& topping) {
         root.toppings.push_back(topping);
+        return *this;
     }
 
     void eat() {
@@ -58,17 +82,41 @@ struct PizzaBuilder {
 };
 
 struct PizzaDirector {
-    Pizza& makePizza(const PizzaBuilder& builder){}
-    Pizza& makeDeepDishPizza(const PizzaBuilder& builder){}
-    Pizza& makeCalzone(const PizzaBuilder& builder){}
-    Pizza& makeDessertPizza(const PizzaBuilder& builder){}
+private :
+    PizzaBuilder builder;
+
+public:
+    PizzaDirector(PizzaBuilder& builder) : builder(builder){}
+
+    Pizza& makeHawiaan () {
+        builder.addBase(Base::sourdough);
+        builder.addSauce("Tomato");
+        builder.addCheese("Mozzarella");
+        builder.addSize(Size::medium);
+        builder.addTopping("Pineapple").addTopping("Ham");
+        return builder.root;
+    }
+
+    Pizza& makeDessertPizza() {
+        builder.addBase(Base::normal);
+        builder.addSauce("Nutella");
+        builder.addCheese("Mascapone");
+        builder.addSize(Size::small);
+        builder.addTopping("Biscoff Cookies").addTopping("Lindor Truffles");
+        return builder.root;
+    }
 };
 
 int main() {
-    PizzaBuilder builder(Base::normal,"Mozzarella","Tomato",Size::medium);
+    /*PizzaBuilder builder(Base::normal,"Mozzarella","Tomato",Size::medium);
     builder.addTopping("Pineapple");
     builder.addTopping("Chicken");
-    builder.eat();
+    builder.eat();*/
+
+
+    PizzaBuilder builder;
+    PizzaDirector director(builder);
+    Pizza hawiaan = director.makeHawiaan();
 
     return 0;
 }
