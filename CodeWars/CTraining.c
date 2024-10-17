69367592192b20abce43d4edbf7b846b28322321
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-18 01:16:42 +0200

    Update CTraining.c

--- CodeWars/CTraining.c
+++ CodeWars/CTraining.c
@@ -1,9 +1,11 @@
+// Given x, fill result array with the first n multiples of x
 void count_by(unsigned x, unsigned n, unsigned result[n]) {
   for(int i = 1; i <= n; i++) {
     result[i-1] = x * i;  
   }
 }
 
+// Given num return the sumation from 1 to num
 int summation(int num) {
   int res = 0;
   int index = 1;
@@ -12,4 +14,9 @@ int summation(int num) {
     index++;
   } while (index <= num);
   return res;
+}
+
+//Return the additive inverse of num
+float opposite(float num) {
+  return num * -1;
 }
