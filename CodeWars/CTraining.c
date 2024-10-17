a1d8420030e72879aac75b69c28f7748734f67c2
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-18 01:56:59 +0200

    Update CTraining.c

--- CodeWars/CTraining.c
+++ CodeWars/CTraining.c
@@ -80,3 +80,9 @@ const char* even_or_odd(int number)
   return "Odd";
 }
 
+// Refactored
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+const char* even_or_odd(int number)
+{
+  return (number % 2) ? "Odd" : "Even";
+}
