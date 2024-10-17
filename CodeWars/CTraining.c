604411bc521b1d3f2d3193123bb2fff91a5d72b3
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-18 01:21:35 +0200

    Update CTraining.c

--- CodeWars/CTraining.c
+++ CodeWars/CTraining.c
@@ -1,3 +1,14 @@
+#include <stddef.h>
+
+// return a *new, dynamically allocated* array with each element doubled.
+int *maps(const int *arr, size_t size){
+   int * result = malloc(size * sizeof(int));
+  for(int i = 0; i < size; i++){
+    result[i] = arr[i] * 2;
+  }
+  return result;
+}
+
 // Given x, fill result array with the first n multiples of x
 void count_by(unsigned x, unsigned n, unsigned result[n]) {
   for(int i = 1; i <= n; i++) {
