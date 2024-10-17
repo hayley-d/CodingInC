90c0c1d27d86dcef81024a91d84ed174db04ae74
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2024-10-18 01:13:57 +0200

    Update CTraining.c

--- CodeWars/CTraining.c
+++ CodeWars/CTraining.c
@@ -3,3 +3,13 @@ void count_by(unsigned x, unsigned n, unsigned result[n]) {
     result[i-1] = x * i;  
   }
 }
+
+int summation(int num) {
+  int res = 0;
+  int index = 1;
+  do{
+    res += index;
+    index++;
+  } while (index <= num);
+  return res;
+}
